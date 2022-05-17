56a86f6212ad1b66ffc0ac8fd49b9e5ea97b5109
amlanstudio <sarah.ngotta@yahoo.fr>
2022-05-17 21:21:52 +0200

    Page AddGoodPlan

---
+++ qg_website/addGoodPlan.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="Plateforme de bons plans, partage, étudiants, écologies, troc, astuces">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Copyright" content="Equipe du QG - IMAC 1">
+    <meta name="Language" content="Fr">
+    <meta name="Category" content="HTML - CSS">
+    <meta name="Keywords" content="HTML, bons plans, étudiants, IMAC, Champs sur Marne">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="img/leQG_Flavicon.png">
+    <link rel="stylesheet" href="addGoodPlan-style.css">
+    <link rel="stylesheet" href="polices.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+   
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+
+
+     <!-- POLICES -->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&family=Koulen&family=Rubik:ital,wght@0,300;0,400;0,600;0,700;1,300&display=swap" rel="stylesheet">
+    <!-- FIN POLICES -->
+   
+    <title>Le QG - Ajouter un bon plan</title>
+
+</head>
+<body>
+        <nav class="navbar fixed-top  ">
+
+                <div class="d-flex flex-column align-items-center">
+                    <br>
+                    <a href="index.php"><img src="img/leQG_logo.png" width="500" class=" invert img-fluid  " alt="Accueil LE QG"></a>
+                </div>
+
+                <div class="row d-none d-sm-block">
+                    <div class="res  align-items-center ">
+                        <a class="home " href="index.php" ><img class="invert"  src="img/accueil.png" alt="Accueil" width="50"></a>
+                        <a class="chat" href="#" ><img  class="invert"  src="img/chat.png" alt="Chat" width="50"></a>
+                        <a class="connexion" href="seconnecter.php" ><img class="invert"  src="img/sidentifier.png" alt="Connexion" width="50"></a>
+                    </div>
+                </div>
+        </nav>
+
+        <header>
+
+                <div class="ban container md-4 col-12">
+                        <h1>JE SOUHAITE AJOUTER UN BON PLAN !</h1>
+                </div>
+
+
+        </header>
+
+        <main>
+                <div class=" addpage">
+                    <div class="formajoutbp">
+                            <!-- Bons Plans -->
+                            <form method='POST' action="addgoodplan" enctype="multipart/form-data">
+                                    <div class="container">
+                                        <div>
+                                            <label for="title">Titre :</label>
+                                            <input class="nomzone" type="text" placeholder="Nom du Bon Plan" name="title" required>
+                                        </div>
+                                        <div>
+                                            <label for="textContent">Contenu :</label>
+                                            <textarea type="text" placeholder="Description" name="textContent" required></textarea>
+                                        </div>
+                                        <div>
+                                            <label for="startingDate">Date de début :</label>
+                                            <input type="date" placeholder="Entrez la date de début" name="startingDate" required>
+                                        </div>
+                                        <div>
+                                            <label for="endingDate">Date de fin :</label>
+                                            <input type="date" placeholder="Entrez la date de début" name="endingDate" required>
+                                        </div>
+
+                                        <div>
+                                        <label for="category">Catégorie :</label>
+                                        <select name="categories">
+                                        <?php
+                                            foreach($datatab[0] as $category){
+                                                echo '<option value="'.$category['categoryID'].'">'.$category['title'].'</option>';
+                                            }
+                                        ?>
+                                        </select>
+                                        </div>
+                                        
+                                        <div>
+                                        <label for="city">Ville :</label>
+                                        <select name="cities">
+                                        <?php
+                                            foreach($datatab[1] as $city){
+                                                echo '<option value="'.$city['cityID'].'">'.$city['name'].'</option>';
+                                            }
+                                        ?>
+                                        <option value="">En ligne</option>
+                                        </select>
+                                        </div>
+
+                                        <div>
+                                        <label for="media">Photo :</label>
+                                        <input type="file" name="media" accept="image/*"/>
+                                        </div>
+
+                                        <div class="envoiaddBP">
+                                            <button type="submit">POSTER UN NOUVEAU BON PLAN</button>
+                                        </div>
+                                    </div>
+                                </form>
+
+                    </div>
+                    <div class="illustration">
+                        <img src="img/goodplanillu.png" alt="">
+                    </div>
+                </div>
+                                               
+        </main>
+
+        <!-- Footer -->
+<footer class="page-footer container-fluid">
+
+<!-- Footer Links -->
+<div class="container">
+
+  <div class=" navfooter row d-flex  pt-5 mb-3">
+
+        <div class="col-md-2 mb-3">
+            <h6><a href="index.php">ACCUEIL</a></h6>
+        </div>
+
+        <div class="col-md-2 mb-3">
+            <h6><a href="#">MON COMPTE</a></h6>
+        </div>
+
+        <div class="col-md-2 mb-3">
+            <h6><a href="#">MESSAGERIE</a></h6>
+        </div>
+
+        <div class="col-md-2 mb-3">
+            <h6><a href="#">L'ÉQUIPE</a></h6>
+    </div>
+  </div>
+
+  <div class=" row d-flex mb-md-0 mb-4">
+
+    <div class=" presentation col-md-8 col-12 mt-5">
+          <p class="pitch">" Plus qu’un site de bon plans, LE QG est un véritable réseau social où chacun d'entre vous pourra contribuer et faire de cette plateforme un concentré d’informations pertinentes pour vous tous, étudiants de France ! "</p>
+          <p class="signature"> L'Équipe du QG</p>
+
+          <!-- Boutons -->
+          <div class="boutonsfoot">
+              <a class=" inscription btn btn-primary " href="seconnecter.php" role="button">REJOINDRE LE QG !</a>
+          </div>
+    </div>
+  </div>
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">
+    <p>© 2022 Copyright LE QG  - IMAC 1 LLMNP -  <a href="#" target="blank">Mentions Légales</a>
+    </p>
+  </div>
+  <!-- Copyright -->
+</div>
+</footer>
+
+<!-- Footer -->
+
+</body>
+</html>
