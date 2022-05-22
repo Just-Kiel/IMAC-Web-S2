79b4869466885b9ceb65b43fd8a10419f80d0872
odewitness <audepora82@orange.fr>
2022-05-22 12:12:08 +0200

    h2 -> h1

--- views/moncompte-vue-externe.php
+++ views/moncompte-vue-externe.php
@@ -153,7 +153,7 @@
                     </div>
                 </div>
 
-                <h2 class="sesbonsplans">Ses bons plans :</h2>
+                <h1>Ses bons plans :</h1>
 
                 <?php
                     foreach($datatab[1] as $goodplan){
