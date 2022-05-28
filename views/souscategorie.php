b3756fd465c8d7af621b51e84593e903312b95d8
Aurore <82760936+Just-Kiel@users.noreply.github.com>
2022-05-28 21:20:01 +0200

    :bug: Fix des filtres

--- views/souscategorie.php
+++ views/souscategorie.php
@@ -277,7 +277,7 @@
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Filtrer les catégories par :</h5>
+              <h5 class="modal-title" id="exampleModalLabel">Filtrer les bons plans :</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
